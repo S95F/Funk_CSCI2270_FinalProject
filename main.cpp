b732b3aad34e35f2ce55f171b9b4b3da2ddc0d89
S95F <stfu2360@colorado.edu>
2015-05-01 12:19:21 -0600

    Update main.cpp
    added more examples of the functions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,90 @@
 #include <iostream>
+#include <stdlib.h>
 #include "fanalysis.h"
 
 using namespace std;
 
 int main()
 {
-    fanalysis edit("test.txt");
-    edit.listallwords();
+    bool quit = false;
+    
+    string input;
+    cout << "Word Analysis" << endl;
+    cout << "Please enter a text file name (inputing 'default' uses file that came with this):";
+    getline(cin,input);
+    if(input == "default"){
+        fanalysis edit("test.txt");
+    }
+    else{
+        fanalysis edit(input);
+    }
+    while(!quit and edit.get_open()){
+        cout << "Menu:" << endl
+        << "1. Frequent words" << endl
+        << "2. Get total number of words" << endl
+        << "3. Remove punctuation" << endl
+        << "4. Capitlization" << endl
+        << "5. Replace word" << endl
+        << "6. Shift ASCII values" << endl
+        << "7. Make all words backwards" << endl
+        << "8. Get frequency of word" << endl
+        << "9. List words" << endl;
+        getline(cin,input);
+        if(input == "1"){
+            cout << "How big do you want the list:";
+            getline(cin, input);
+            words *frqnt_words = edit.frequent_words(atoi(input.c_str()))'
+            for(int i = 0; i < atoi(input.c_str()); i++){
+                cout << frqnt_words[i].word << endl;
+            }
+        }
+        if(input == "2"){
+            cout << edit.getTotal_num_words() << endl;
+        }
+        if(input == "3"){
+            edit.removePunc();
+            cout << "Punctuation removed" << endl;
+        }
+        if(input == "4"){
+            cout << "Would you like to make it all caps or all lowercase (true for caps, false for lowercase):";
+            getline(cin,input);
+            if(input == "true"){
+                edit.capitalize(true);
+            }
+            if(input == "false"){
+                edit.capitalize(false);
+            }
+        }
+        if(input == "5"){
+            string replace;
+            cout << "What word would you like to replace:";
+            getline(cin,input);
+            cout << "What word would you like to replace it with:";
+            getline(cin,replace);
+            edit.replaceWrd(input,replace);
+        }
+        if(input == "6"){
+            cout << "How much would you like to shift the ASCII values:";
+            getline(cin,input);
+            edit.shift_all_words(atoi(input.c_str()));
+            cout << "Values shifted" << endl;
+        }
+        if(input == "7"){
+            edit.backwords_words();
+            cout << "The words are now backwards" << endl;
+        }
+        if(input == "8"){
+            cout << "Which word would you like to know the frequency of:";
+            getline(cin,input);
+            cout << "Frequency of " << input << " is " << edit.frequencyofword(input) << "." << endl;
+        }
+        if(input == "9"){
+            words *allwords = edit.listallwords();
+            for(int i = 0; i < edit.getTotal_num_words(); i++){
+                cout << allwords[i].word << endl;
+            }
+        }
+    }
     return 0;
 
 }
